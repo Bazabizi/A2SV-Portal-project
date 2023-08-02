8f09ee57401791db7017e077e0aa38818d0ddf13
Natnael Tafesse <bazabizi3@gmail.com>
2023-08-02 16:37:16 +0300

    Model class

---
+++ Task.dart
@@ -0,0 +1,11 @@
+
+class Task {
+  String title;
+  String description;
+  DateTime dueDate;
+  bool status;
+
+  
+  Task({required this.title , required this.description , required this.dueDate , required this.status});
+  
+}
